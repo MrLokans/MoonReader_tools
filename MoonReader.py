cb39d2805c8ff9261298d6af2c0a573cecbb7c27
MrLokans <trikster1911@gmail.com>
2015-12-30 22:23:35 +0300

    Added PDF notes parser.

--- MoonReader.py
+++ MoonReader.py
@@ -2,6 +2,16 @@ import re
 import os
 import zlib
 
+NOTE_EXTENSION = ".an"
+STAT_EXTENSION = ".po"
+
+
+def one_obj_or_list(seq):
+    """If there is one object in list - return object, either return list"""
+    if len(seq) == 1:
+        return seq[0]
+    return seq
+
 
 class AbstractNote(object):
 
@@ -10,28 +20,121 @@ class AbstractNote(object):
     UNDERLINE = 0b100
     WAVED = 0b001
 
-    def __init__(self, note_id=0, note_text="", note_time=None, note_color=(), note_modifier=0b0):
+    def __init__(self, note_id=0, note_text="", note_timestamp=None, note_color=(), note_modifier=0b0):
         self.id = note_id
         self.text = note_text
-        self.time = note_time
-        self.color = note_color
+        self._timestamp = note_timestamp
+        self._color = note_color
         self.modifier = note_modifier
+
+    @property
+    def time(self):
+        return self._timestamp
+
+    def _color_from_number(self):
+        return self._color
+
+
+class AbstractBookNotes(object):
+
+    def __init__(self, id, notes):
+        self.id = id
+        self.notes = notes
 
 
 class PDF_Note_Parser(object):
-    def __init__(self, text):
-        pass
+
+    NOTE_START = "#A*#"
+    NOTE_END = "#A@#"
+
+    def __init__(self, id, notes):
+        self.id = id
+        self.notes = notes
+
+    @staticmethod
+    def from_text(text):
+        note_texts = PDF_Note_Parser._find_note_text_pieces(text)
+        notes = PDF_Note_Parser._notes_from_note_texts(note_texts)
+        return PDF_Note_Parser(id=None, notes=notes)
+
+    @staticmethod
+    def _find_note_text_pieces(text):
+        notes = []
+
+        _text = text
+        while(_text):
+            start_pos = _text.find(PDF_Note_Parser.NOTE_START)
+            end_pos = _text.find(PDF_Note_Parser.NOTE_END)
+            if start_pos != -1 and end_pos != -1:
+                note_text = _text[start_pos:end_pos+len(PDF_Note_Parser.NOTE_END)]
+                notes.append(note_text)
+            else:
+                break
+            _text = _text[end_pos+len(PDF_Note_Parser.NOTE_END):]
+        return notes
+
+    @staticmethod
+    def _notes_from_note_texts(note_texts):
+        return [PDF_Note.from_text(text) for text in note_texts]
 
 
 class PDF_Note(AbstractNote):
-    pass
 
+    SPLITTER_PATTERN = r"#A[0-9@\*]#"
+    CORRESP_TABLE = (
+        (0, None),
+        (1, "page"),
+        (2, "timestamp"),
+        (3, None),
+        (4, None),
+        (5, "color"),
+        (6, "style"),
+        (7, None),
+        (8, "text"),
+        (9, None)
+    )
 
-def one_obj_or_list(seq):
-    """If there is one object in list - return object, either return list"""
-    if len(seq) == 1:
-        return seq[0]
-    return seq
+    STYLE_CORRESP = {
+        "0": AbstractNote.MARKER,
+        "1": AbstractNote.UNDERLINE,
+        "2": AbstractNote.CROSSED,
+        "3": AbstractNote.WAVED,
+    }
+
+    def __init__(self, text, timestamp, style, color):
+        super(PDF_Note, self).__init__(note_id=0,
+                                       note_text=text,
+                                       note_timestamp=timestamp,
+                                       note_modifier=style,
+                                       note_color=color)
+
+    @staticmethod
+    def from_text(text):
+        token_dict = PDF_Note._dict_from_text(text)
+
+        style = PDF_Note._style_from_num_str(token_dict["style"])
+
+        return PDF_Note(text=token_dict.get("text", ""),
+                        timestamp=token_dict.get("timestamp", ""),
+                        style=style,
+                        color=token_dict.get("color", ""))
+
+    @staticmethod
+    def _dict_from_text(text):
+        """Split note's text according to regex, and return fields"""
+
+        note_tokens = re.split(PDF_Note.SPLITTER_PATTERN, text)
+        assert len(note_tokens) > 8
+        d = {}
+        for item in PDF_Note.CORRESP_TABLE:
+            if not item[1]:
+                continue
+            d[item[1]] = note_tokens[item[0]]
+        return d
+
+    @staticmethod
+    def _style_from_num_str(num_str):
+        return PDF_Note.STYLE_CORRESP[num_str]
 
 
 class FB2_Note(AbstractNote):
@@ -53,9 +156,9 @@ class FB2_Note(AbstractNote):
         (13, 3, 'modifier_bits')
     ]
 
-    def __init__(self, note_id, note_text, note_time, note_color, note_modifier):
+    def __init__(self, note_id, note_text, note_timestamp, note_color, note_modifier):
         super(FB2_Note, self).__init__(note_id, note_text,
-                                       note_time, note_color, note_modifier)
+                                       note_timestamp, note_color, note_modifier)
 
     @staticmethod
     def from_str_list(str_list):
@@ -64,13 +167,14 @@ class FB2_Note(AbstractNote):
             d[item[2]] = str_list[item[0]:item[0]+item[1]]
         return FB2_Note(note_id=one_obj_or_list(d["id"]),
                         note_text=one_obj_or_list(d["text"]),
-                        note_time=one_obj_or_list(d["timestamp"]),
+                        note_timestamp=one_obj_or_list(d["timestamp"]),
                         note_color=one_obj_or_list(d["color"]),
                         note_modifier=FB2_Note.modifier_from_seq(d["modifier_bits"])
                         )
 
     @staticmethod
     def modifier_from_seq(seq):
+        """Transform a sequence of zeros and ones into binary number"""
         return int("".join(map(str, seq)), base=2)
 
 
@@ -126,8 +230,9 @@ class MoonReaderNotes(object):
         "fb2": FB2_Note_Parser,
     }
 
-    def __init__(self, notes):
-        self.notes = notes
+    # def __init__(self, id=id, notes=notes):
+    #     self.id = id
+    #     self.notes = notes
 
     @staticmethod
     def from_file(file_path):
