c5f0ebbc8c826469cabbfaba64691566905c2f30
MrLokans <trikster1911@gmail.com>
2016-01-02 12:50:41 +0300

    Added color and date parsing methods.

--- MoonReader.py
+++ MoonReader.py
@@ -1,6 +1,8 @@
 import re
 import os
 import zlib
+import struct
+import datetime
 
 NOTE_EXTENSION = ".an"
 STAT_EXTENSION = ".po"
@@ -11,6 +13,58 @@ def one_obj_or_list(seq):
     if len(seq) == 1:
         return seq[0]
     return seq
+
+
+def get_moonreader_files(path):
+    files = map(lambda x: os.path.join(path, x), os.listdir(path))
+    return filter(lambda x: x.endswith((NOTE_EXTENSION, STAT_EXTENSION)), files)
+
+
+def date_from_long_timestamp(str_timestamp):
+    return datetime.datetime.fromtimestamp(float(str_timestamp[:10]))
+
+
+def rgba_hex_from_int(number):
+    number = int(number)
+    # color is stored in overflowed integer representation (yeap, that's weird)
+    byte_form = struct.pack(">i", number)
+    byte_values = map(hex, byte_form)
+    return list(byte_values)
+
+
+def rgb_string_from_hex(hex_seq):
+    hex_seq = hex_seq[-3:]
+    str_form = map(lambda x: x.replace('0x', '').upper(), hex_seq)
+    return '#' + "".join(str_form)
+
+
+def get_same_book_files(files):
+    """Returns pairs of files that belong to the same book"""
+    pairs = []
+    files = list(files)
+    files_set = set(files)
+    for fname in files:
+        if fname not in files_set:
+            continue
+        if fname.endswith(NOTE_EXTENSION):
+            pair_fname = os.path.splitext(fname)[0] + STAT_EXTENSION
+            if pair_fname in files_set:
+                pairs.append((fname, pair_fname))
+                files_set.remove(fname)
+                files_set.remove(pair_fname)
+            else:
+                pairs.append((fname, ""))
+                files_set.remove(fname)
+        elif fname.endswith(STAT_EXTENSION):
+            pair_fname = os.path.splitext(fname)[0] + NOTE_EXTENSION
+            if pair_fname in files_set:
+                pairs.append((pair_fname, fname))
+                files_set.remove(fname)
+                files_set.remove(pair_fname)
+            else:
+                pairs.append(("", fname))
+                files_set.remove(fname)
+    return pairs
 
 
 class AbstractNote(object):
@@ -29,7 +83,7 @@ class AbstractNote(object):
 
     @property
     def time(self):
-        return self._timestamp
+        return date_from_long_timestamp(self._timestamp)
 
     def _color_from_number(self):
         return self._color
@@ -286,7 +340,9 @@ class MoonReaderStatistics(object):
 
 
 def main():
-    pass
+    colors = [-6706569, 1795227647, -11184811]
+    for col in colors:
+        print(rgba_hex_from_int(col))
 
 if __name__ == '__main__':
     main()
