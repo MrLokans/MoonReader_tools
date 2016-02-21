421d3844950c41fc185a5911a6f1e08f7c052422
MrLokans <trikster1911@gmail.com>
2016-02-21 11:31:57 +0300

    Setup.py updated. CLI entry point added. README updated.

---
+++ README.md
@@ -0,0 +1,11 @@
+Installation
+============
+```bash
+python setup.py install
+```
+
+Usage
+=====
+```bash
+moonreader_tools --path <path/to/moonreader/cache> --output-file <outfile>.json
+```
--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
     name="moonreader_tools",
-    version='0.3.2',
+    version='0.4.2',
     description="Set of class and utilities to parse MoonReader+ book-related files.",
     url='MoonReader_tools',
     author='MrLokans',
     license='MIT',
     packages=['moonreader_tools'],
+    entry_points={
+        'console_scripts': [
+            'moonreader_tools = moonreader_tools.main:main'
+        ]
+    },
     test_suite="tests.test_all",
     zip_safe=False
 )
