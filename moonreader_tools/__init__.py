90dd42b7006f97215ac73658f1173515f4a59725
MrLokans <trikster1911@gmail.com>
2016-05-23 21:40:49 +0300

    Metadata added to setup.py.

---
+++ moonreader_tools/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'requests'
+__version__ = '0.8.8'
+__author__ = 'MrLokans'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 MrLokans'
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+import re
 from setuptools import setup, find_packages
+
+version = ""
+
+# I actually took it from requests library
+with open('requests/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise ValueError("No version specified.")
 
 setup(
     name="moonreader_tools",
-    version='0.8.7',
+    version=version,
     description=("Set of class and utilities to parse"
                  "MoonReader+ book-related files.",
                  "It is capable to reading, creating handy object"
                  "representation and writing notes and statistics files")[0],
     url='https://github.com/MrLokans/MoonReader_tools/',
+    download_url='https://github.com/MrLokans/MoonReader_tools/tarball/{}'.format(version),
     author='MrLokans',
+    author_email='mrlokans@gmail.com',
     license='MIT',
     packages=find_packages(exclude=["tests"]),
     entry_points={
@@ -19,6 +32,18 @@ setup(
     install_requires=[
         'dropbox',
     ],
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ),
     test_suite="tests.test_all",
     zip_safe=False
 )
